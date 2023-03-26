ab346fa6c821d6a1a5bcb51ad4f47f6572b57008
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-03-26 18:20:08 -0400

    Content looks good

--- index.php
+++ index.php
@@ -36,13 +36,44 @@
     </header>
     <main>
         <section>
-            <h2>Welcome to my website!</h2>
+
+		<div class="content-column">
+    		<h3>Content Column - Title</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id faucibus nisl tincidunt eget nullam non. Elit ut aliquam purus sit amet luctus. Eget lorem dolor sed viverra ipsum nunc. Nec feugiat nisl pretium fusce. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Tortor at risus viverra adipiscing at in. Suspendisse potenti nullam ac tortor vitae purus. Tortor condimentum lacinia quis vel eros donec ac odio tempor. In mollis nunc sed id semper risus in hendrerit. Libero justo laoreet sit amet cursus. Risus nec feugiat in fermentum posuere urna nec tincidunt. Et odio pellentesque diam volutpat. Potenti nullam ac tortor vitae purus. Est lorem ipsum dolor sit amet consectetur adipiscing elit. Urna molestie at elementum eu facilisis sed odio morbi quis. Aliquam ultrices sagittis orci a scelerisque purus semper. Massa enim nec dui nunc.</p>
 			<p>Viverra maecenas accumsan lacus vel facilisis. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Elementum pulvinar etiam non quam lacus suspendisse. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Molestie at elementum eu facilisis sed odio morbi quis. Ut tellus elementum sagittis vitae et leo duis ut. Senectus et netus et malesuada fames ac. Maecenas sed enim ut sem viverra aliquet eget sit. Porttitor leo a diam sollicitudin tempor id eu nisl. Ut ornare lectus sit amet est placerat in egestas. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Tristique risus nec feugiat in fermentum. Quis viverra nibh cras pulvinar mattis. Urna condimentum mattis pellentesque id nibh tortor id. Blandit massa enim nec dui nunc mattis. Tincidunt dui ut ornare lectus sit amet est placerat. Orci sagittis eu volutpat odio.</p>
 			<p>In est ante in nibh mauris. Elit ullamcorper dignissim cras tincidunt lobortis. Odio morbi quis commodo odio aenean sed adipiscing. Massa enim nec dui nunc mattis enim. Vitae aliquet nec ullamcorper sit amet risus. Quam lacus suspendisse faucibus interdum posuere lorem ipsum. In cursus turpis massa tincidunt dui ut. Eget dolor morbi non arcu risus quis varius quam. Sit amet dictum sit amet justo donec enim diam vulputate. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Pellentesque pulvinar pellentesque habitant morbi tristique. Sed augue lacus viverra vitae congue eu consequat ac felis.
-        </section>
+		</div>
+		<div class="sidebar-column">
+			<div class="widget">
+			<h3>Sidebar Column</h3>
+			<h4>Widget 1</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam eget interdum malesuada, mauris est rutrum velit, vel pulvinar augue eros eu justo. Aenean blandit arcu sit amet erat bibendum tempus.</p>
+			</div>
+
+			<div class="widget">
+			<h3>Sidebar Column</h3>
+			<h4>Widget 2</h4>
+			<p>Nulla id lorem ac erat hendrerit finibus. Sed sit amet nulla lectus. Fusce hendrerit dui ut lorem lobortis lobortis. Donec ac ex euismod, faucibus leo vel, ultrices sapien. Donec tincidunt enim at dolor maximus imperdiet.</p>
+			</div>
+	
+			<div class="widget">
+			<h3>Sidebar Column</h3>
+			<h4>Widget 3</h4>
+			<p>Nulla id lorem ac erat hendrerit finibus. Sed sit amet nulla lectus. Fusce hendrerit dui ut lorem lobortis lobortis. Donec ac ex euismod, faucibus leo vel, ultrices sapien. Donec tincidunt enim at dolor maximus imperdiet.</p>
+			</div>
+
+			<!--	
+			<div class="widget">
+			<h3>Sidebar Column</h3>
+			<h4>Widget 4</h4>
+			<p>Nulla id lorem ac erat hendrerit finibus. Sed sit amet nulla lectus. Fusce hendrerit dui ut lorem lobortis lobortis. Donec ac ex euismod, faucibus leo vel, ultrices sapien. Donec tincidunt enim at dolor maximus imperdiet.</p>
+			</div>
+			-->
+
+		</section>
     </main>
     <footer>
         <p>&copy; 2023 My Page. All rights reserved.</p>
     </footer>
+</body>
 	
