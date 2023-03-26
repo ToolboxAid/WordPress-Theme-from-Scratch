c655523e042901a87b288131887bc7831216b4fc
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-03-26 18:40:24 -0400

    footer looks good enough to continue to setup WP pages

--- index.php
+++ index.php
@@ -72,8 +72,58 @@
 
 		</section>
     </main>
-    <footer>
-        <p>&copy; 2023 My Page. All rights reserved.</p>
-    </footer>
+
+	<footer>
+		<div class="footer-row">
+			<div class="footer-col">
+			<h4>Site Pages</h4>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+			</div>
+			<div class="footer-col">
+			<h4>Products</h4>
+			<ul>
+				<li><a href="#">Product A</a></li>
+				<li><a href="#">Product B</a></li>
+				<li><a href="#">Product C</a></li>
+				<li><a href="#">Product D</a></li>
+			</ul>
+			</div>
+			<div class="footer-col">
+			<h4>Company</h4>
+			<ul>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Jobs</a></li>
+				<li><a href="#">Press</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+			</div>
+			<div class="footer-col">
+			<h4>Legal</h4>
+			<ul>
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">Terms and Conditions</a></li>
+				<li><a href="#">Cookie Policy</a></li>
+				<li><a href="#">Disclaimer</a></li>
+			</ul>
+			</div>
+		</div>
+		<div class="footer-row">
+			<div class="footer-col">
+			<!--<img src="footer-image.jpg" alt="Footer Image">-->
+			<hr/>
+			</div>
+		</div>
+		<div class="footer-row">
+			<div class="footer-col">
+			<p>&copy; 2023 Company Name. All rights reserved.</p>
+			</div>
+		</div>
+	</footer>
+
 </body>
 	
