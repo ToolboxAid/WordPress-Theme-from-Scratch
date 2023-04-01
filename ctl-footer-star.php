d99307cc2f530d5a2b5902ff37e6c7ca8ae55142
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-04-01 17:35:47 -0400

    Star looks good

--- ctl-footer-star.php
+++ ctl-footer-star.php
@@ -7,7 +7,7 @@ function footer_star( $wp_customize ) {
 
 	/* Create section */
 	$wp_customize->add_section('footer_star', array(
-		'title' => __('Footer - Line (incomplete)', 'qbytesworld_WordPress'),
+		'title' => __('Footer - Star', 'qbytesworld_WordPress'),
 		'priority' => 168,
 	));
 
@@ -15,54 +15,79 @@ function footer_star( $wp_customize ) {
     /* ************************************************************ */
     /* ************************************************************ */
 
-    // Add checkbox to hide title & tagline 
-    $wp_customize->add_setting( 'footer_star_sub_checkbox', array(
-        'default'   => false,
-        'transport' => 'refresh',
-    ) );
-    $wp_customize->add_control( 'footer_star_sub_checkbox', array(
-        'label'    => __( 'Hide Title & Tagline', 'qbytesworld_WordPress' ),
-        'section'  => 'footer_star',
-        'settings' => 'footer_star_checkbox',
-        'description' => __( 'True/False', 'qbytesworld_WordPress' ),        
-        'type'     => 'checkbox',
-    ) );	
-	
-    /* Button color */
+    /* Star color */
 	$wp_customize->add_setting('footer_star_color', array(
-		'default' => '#004C87',
+		'default' => '#ffff01',
 		'transport' => 'refresh',
 	));
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_star_color_control', array(
-		'label' => __('Button Color', 'qbytesworld_WordPress'),
+		'label' => __('Star Color', 'qbytesworld_WordPress'),
 		'section' => 'footer_star',
 		'settings' => 'footer_star_color',
         'description'=> __( 'Color for Menu Item', 'qbytesworld_WordPress' ),        
 	) ) );
 
-    /* Button hover color */
-	$wp_customize->add_setting('footer_star_color_hover', array(
-		'default' => '#004C87',
-		'transport' => 'refresh',
-	));
-	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_star_color_hover_control', array(
-		'label' => __('Button Hover Color', 'qbytesworld_WordPress'),
-		'section' => 'footer_star',
-		'settings' => 'footer_star_color_hover',
-        'description'=> __( 'Mouse Menu Item Hover color', 'qbytesworld_WordPress' ),        
-	) ) );
 
-    /* Button hover color */
-	$wp_customize->add_setting('footer_star_color_active', array(
-		'default' => '#004C87',
-		'transport' => 'refresh',
-	));
-	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'footer_star_color_active_control', array(
-		'label' => __('Button Active Color', 'qbytesworld_WordPress'),
-		'section' => 'footer_star',
-		'settings' => 'footer_star_color_active',
-        'description'=> __( 'Active Menu Item color', 'qbytesworld_WordPress' ),        
-	) ) );
+    /* ************************************************************ */
+    // font size size
+    $wp_customize->add_setting( 'footer_star_font_size', array(
+        'default'    => '50',
+        'transport'  => 'refresh',
+     ) );
+     $wp_customize->add_control( 'footer_star_font_size_control', array(
+        'section'    => 'footer_star',
+        'settings'   => 'footer_star_font_size',
+        'label'      => __( 'Font size', 'qbytesworld_WordPress' ),
+        'description'=> __( 'Adjust the font size of the star', 'qbytesworld_WordPress' ),        
+        'type'       => 'range',
+        'priority'   => 10,
+        'input_attrs' => array(
+        'min'    => '0',
+        'max'    => '100',
+        'step'   => '1',
+        ),
+     ) );
+
+    /* ************************************************************ */
+    // Offset size
+    $wp_customize->add_setting( 'footer_star_offset', array(
+        'default'    => '30',
+        'transport'  => 'refresh',
+     ) );
+     $wp_customize->add_control( 'footer_star_offset_control', array(
+        'section'    => 'footer_star',
+        'settings'   => 'footer_star_offset',
+        'label'      => __( 'Offset size', 'qbytesworld_WordPress' ),
+        'description'=> __( 'Adjust the offset size of the star', 'qbytesworld_WordPress' ),        
+        'type'       => 'range',
+        'priority'   => 10,
+        'input_attrs' => array(
+        'min'    => '0',
+        'max'    => '80',
+        'step'   => '1',
+        ),
+     ) );
+	 
+	     /* ************************************************************ */
+    // Border size
+    $wp_customize->add_setting( 'footer_star_hr_size', array(
+        'default'    => '4',
+        'transport'  => 'refresh',
+     ) );
+     $wp_customize->add_control( 'footer_star_hr_size_control', array(
+        'section'    => 'footer_star',
+        'settings'   => 'footer_star_hr_size',
+        'label'      => __( 'Star HR size', 'qbytesworld_WordPress' ),
+        'description'=> __( 'Adjust the border size of the star', 'qbytesworld_WordPress' ),        
+        'type'       => 'range',
+        'priority'   => 10,
+        'input_attrs' => array(
+        'min'    => '0',
+        'max'    => '10',
+        'step'   => '1',
+        ),
+     ) );
+
 
 }
 add_action('customize_register', 'footer_star');
@@ -75,20 +100,20 @@ function footer_star_css() { ?>
 
 	<style type="text/css">
 		footer .star{
-			font-size: 55px;
-			color: yellow;
+			font-size: <?php echo get_theme_mod('footer_star_font_size'); ?>px;
+			color: <?php echo get_theme_mod('footer_star_color'); ?>;
 
 			margin: 0px;
 			display: flex; 
 			align-items: center;	
 		}
 		footer .star i{
-			padding-left: 30px;		
-			padding-right: 30px;		
+			padding-left: <?php echo get_theme_mod('footer_star_offset'); ?>px;
+			padding-right: <?php echo get_theme_mod('footer_star_offset'); ?>px;		
 		}		
 		footer .star hr{
-			height: 3px; 
-			background-color: yellow;
+			height: <?php echo get_theme_mod('footer_star_hr_size'); ?>px;
+			background-color: <?php echo get_theme_mod('footer_star_color'); ?>;;
 
 			width: 50%; 
 		}
