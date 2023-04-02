87268c3b0f49adb398dad416dd01013e6fafca8e
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-04-02 17:59:38 -0400

    corrected bad form on the EMPTY

--- page.php
+++ page.php
@@ -8,24 +8,26 @@
 
 	if (have_posts()) :
 		while (have_posts()) : the_post();		
-				if ( has_children() OR $post->post_parent > 0 ) { ?>			
-					<nav class="site-nav children-links clearfix">
-						<span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
-						<ul>
-							<?php
-							$args = array(
-								'child_of' => get_top_ancestor_id(),
-								'title_li' => ''
-							); ?>
-							<?php wp_list_pages($args); ?>
-						</ul>
-					</nav>			
-				<?php } ?>		
-				<h2><?php the_title(); ?></h2>
-				<?php the_content(); ?>
+			if ( has_children() OR $post->post_parent > 0 ) { ?>			
+				<nav class="site-nav children-links clearfix">
+					<span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
+					<ul>
+						<?php
+						$args = array(
+							'child_of' => get_top_ancestor_id(),
+							'title_li' => ''
+						); ?>
+						<?php wp_list_pages($args); ?>
+					</ul>
+				</nav>			
+			<?php } ?>		
+			<h2><?php the_title(); ?></h2>
+			<?php the_content(); ?>
 		<?php endwhile;	
-		else :
-			echo '<p>Sorry, we are too lazy to load any Pages!)</p>';		
+	else :
+		echo '<nav class="site-nav">';
+		echo '<p>Sorry, we are just too lazy to load any Pages!</p>';		
+		echo '</nav>';
 		endif; 
 	?>
 	</article>	
