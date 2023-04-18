2e2d740804f7b036574b96cc5ab8c8df13f77584
Mr. Q <97194984+ToolboxAid@users.noreply.github.com>
2023-04-17 23:16:23 -0400

    starting prev/next

--- single.php
+++ single.php
@@ -14,6 +14,21 @@ debug_location("Single"); ?>
 if (have_posts()) :
 	while (have_posts()) : the_post(); 
 		get_template_part('content');
+
+        // Previous/next post navigation.
+        the_post_navigation(
+            array(
+                'next_text' => __( 'Next post: %title', 'qbytesworld_WordPress' ),
+                'prev_text' => __( 'Previous post: %title', 'qbytesworld_WordPress' ),
+            )
+        );	
+        // the_post_navigation(
+        //     array(
+        //         'next_text' => __( 'Next post: %title', 'qbytesworld_WordPress' ),
+        //         'prev_text' => __( 'Previous post: %title', 'qbytesworld_WordPress' ),
+        //     )
+        // );			
+		
 	endwhile;	
 else : 
 	get_template_part('content-sorry');
