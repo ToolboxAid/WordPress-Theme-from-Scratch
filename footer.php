5f3b60e4dfd3b8c62587d3f0c34e277bdc93a042
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-04-20 19:13:02 -0400

    404 update, footer widget fix,  tagline move down

--- footer.php
+++ footer.php
@@ -5,50 +5,43 @@
 		<footer>		
 			<!-- footer-widgets -->
 			<div class="footer-row footer-row-menu">
-				<div class="footer-col menu"><?php
+				<div class="footer-col menu footer-customer"><?php
 					$footer_menu_name = wp_get_nav_menu_name('footer1');// Get the menu name for 'footer1'
 					echo '<h4>' . $footer_menu_name . '</h4>';// Echo the menu name to an H4 tag
-					 ?>
-								
-						<?php				
-						$args = array( 'theme_location' => 'footer1' ); 
-						wp_nav_menu(  $args );
+					dynamic_sidebar( 'footer1' );										
+						// $args = array( 'theme_location' => 'footer1' ); 
+						// wp_nav_menu(  $args );
 						?>
 					
 				</div>	<!-- site-nav end -->
 
-				<div class="footer-col menu"><?php
+				<div class="footer-col menu footer-site"><?php
 					$footer_menu_name = wp_get_nav_menu_name('footer2');// Get the menu name for 'footer1'
 					echo '<h4>' . $footer_menu_name . '</h4>';// Echo the menu name to an H4 tag
-					 ?>
-					
-						<?php				
-						$args = array( 'theme_location' => 'footer2' ); 
-						wp_nav_menu(  $args );
+					dynamic_sidebar( 'footer2' );										
+						// $args = array( 'theme_location' => 'footer2' ); 
+						// wp_nav_menu(  $args );
 						?>
 					
 				</div>	<!-- site-nav end -->
 
-				<div class="footer-col menu"><?php
+				<div class="footer-col menu footer-legal"><?php
 					$footer_menu_name = wp_get_nav_menu_name('footer3');// Get the menu name for 'footer1'
 					echo '<h4>' . $footer_menu_name . '</h4>';// Echo the menu name to an H4 tag
-					 ?>
-					
-						<?php				
-						$args = array( 'theme_location' => 'footer3' ); 
-						wp_nav_menu(  $args );
+					dynamic_sidebar( 'footer3' );										
+						// $args = array( 'theme_location' => 'footer3' ); 
+						// wp_nav_menu(  $args );
 						?>
 					
 				</div>	<!-- site-nav end -->
 
-				<div class="footer-col menu "><?php
+				<div class="footer-col menu footer-recent"><?php
 					$footer_menu_name = wp_get_nav_menu_name('footer4');// Get the menu name for 'footer1'
 					echo '<h4>' . $footer_menu_name . '</h4>';// Echo the menu name to an H4 tag
-					 ?>
 					
-						<?php				
-						$args = array( 'theme_location' => 'footer4' ); 
-						wp_nav_menu(  $args );
+					dynamic_sidebar( 'footer4' );										
+						// $args = array( 'theme_location' => 'footer4' ); 
+						// wp_nav_menu(  $args );
 						?>
 					
 				</div>	<!-- site-nav end -->
@@ -64,6 +57,7 @@
 					</div>
 				</div>
 			</div>
+			
 			<div class="footer-row  footer-row-copyright">
 				<div class="footer-col align-center copyright">
 					<p>&copy;  
