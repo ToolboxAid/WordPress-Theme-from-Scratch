934f25f928b9c6187348c26f68e742342285faee
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-05-06 16:26:59 -0400

    Talk to your doctor

--- single-exercise-template.php
+++ single-exercise-template.php
@@ -46,7 +46,17 @@ if (have_posts()) :
                     }				
                     echo trim($output, $separator);
                 } ?>
-            </p><?php
+            </p>
+            <div class="tba-message tba-warn">
+                <p>
+                    <i class="fa fa-exclamation-triangle"></i>
+                    Caution and common sense should be used...as these are my experiences, and my unique situations.&nbsp; 
+                    <i>Talk to your doctor.</i>&nbsp; 
+                    They may work for you or they may not.&nbsp; 
+                    You may have different results.&nbsp; Please read our 
+                    <a href="/legal_pages/disclaimer/">disclaimer</a>.
+                </p>
+            </div> <?php
                     $workout_type = get_post_meta( get_the_ID(), 'workout_type', true );
                     if ( $workout_type ) {
                         echo '<p><h3 style="display: inline;">Type: </h3>' . esc_html( $workout_type ) . '</p>';
@@ -128,7 +138,17 @@ the_content(); ?>
                 echo "</style>";
             } ?>
 
-        </div> <?php
+        </div> 
+        <div class="tba-message tba-warn">
+                <p>
+                    <i class="fa fa-exclamation-triangle"></i>
+                    Caution and common sense should be used...as these are my experiences, and my unique situations.&nbsp; 
+                    <i>Talk to your doctor.</i>&nbsp;  
+                    They may work for you or they may not.&nbsp; 
+                    You may have different results.&nbsp; Please read our 
+                    <a href="/legal_pages/disclaimer/">disclaimer</a>.
+                </p>
+            </div> <?php
         get_template_part( 'content-comments' );  ?>
 	</article>
 <?php
